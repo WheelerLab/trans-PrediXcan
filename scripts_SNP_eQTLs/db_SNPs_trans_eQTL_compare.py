86b632bb0330076e3d12a82b88c78112a199ae8c
Angela Andaleon <angie.andaleon@gmail.com>
2019-01-09 15:09:31 -0600

    update for recreating table S3

--- scripts_SNP_eQTLs/db_SNPs_trans_eQTL_compare.py
+++ scripts_SNP_eQTLs/db_SNPs_trans_eQTL_compare.py
@@ -5,6 +5,7 @@ Created on Tue Jan 08 14:47:20 2019
 @author: Angela Andaleon (aandaleon@luc.edu)
 """
 
+import numpy as np
 import sqlite3
 import pandas as pd
 import re
@@ -56,3 +57,25 @@ MultiXcan_trans_eQTL = pd.merge(MultiXcan, db_SNPs_trans_eQTLGen_obsgene_predgen
 MultiXcan_trans_eQTL["trans-eQTL_in_eQTLGen"].fillna("No", inplace = True) #if there's no matching in trans-eQTLGen
 MultiXcan_trans_eQTL.to_csv("MultiXcan_trans_eQTL.csv", index = False, na_rep = "NA")
 print("Completed writing MultiXcan trans-PX and trans-eQTLGen overlapping results to MultiXcan_trans_eQTL.csv")
+
+#Recreate Table S3 - Trans-acting/target gene pairs from FHS MulTiXcan (FDR < 0.05) that were previously identified as trans-eQTLs (FDR < 0.05) in whole blood in Westra et al. (https://molgenis58.target.rug.nl/bloodeqtlbrowser/)
+#\textit{trans}-eQTLs within 1Mb of our PrediXcan and MulTiXcan trans-acting genes with the same target genes are shown in Table S3. 
+#db_SNPs = pd.read_csv("db_SNPs.txt")
+#okay so WHLBLD has predgene and obsgene pairs
+#and trans_eQTLGen_FDR_05 has SNP and obsgene pairs
+#match on: Westra.GENEname == obsname; Westra.SNPchr == predChr
+    #and then restrict Westra.SNPpos to - 1Mb of predS1 and + 1 Mb of predS2
+trans_eQTLGen_FDR_05.columns = ['eQTLGen_P', 'rsid', 'predChr', 'rsid_pos', 'Zscore', 'A1', 'A0', 'obsgene', 'eQTLGen_gene_symbol', 'eQTLGen_gene_chr', 'eQTLGen_gene_pos', 'eQTLGen_nr_cohorts', 'eQTLGen_nr_samples', 'eQTLGen_FDR']
+WHLBLD_match_eQTLGen = pd.merge(WHLBLD, trans_eQTLGen_FDR_05, on = ["predChr", "obsgene"])
+WHLBLD_match_eQTLGen.loc[WHLBLD_match_eQTLGen.rsid_pos <= (WHLBLD_match_eQTLGen.predS1 - 1000000), ['rsid_pos']] = None #keep if w/in +/- 1 Mb https://stackoverflow.com/questions/19226488/change-one-value-based-on-another-value-in-pandas
+WHLBLD_match_eQTLGen.loc[WHLBLD_match_eQTLGen.rsid_pos >= (WHLBLD_match_eQTLGen.predS2 + 1000000), ['rsid_pos']] = None
+WHLBLD_match_eQTLGen = WHLBLD_match_eQTLGen[np.isfinite(WHLBLD_match_eQTLGen['rsid_pos'])] #keep SNPs that pass
+WHLBLD_match_eQTLGen = WHLBLD_match_eQTLGen[["eQTLGen_P", "rsid", "predChr", "rsid_pos", "eQTLGen_gene_chr", "eQTLGen_gene_pos", "eQTLGen_gene_symbol", "eQTLGen_FDR", "predgene", "predname", "predS1", "obsgene", "obsname", "obsS1", "obsS2", "FHS_pval", "FHS_FDR", "DGN_pval"]] #keep important cols.
+WHLBLD_match_eQTLGen.to_csv("WHLBLD_match_eQTLGen.csv", index = False)
+
+MultiXcan_match_eQTLGen = pd.merge(MultiXcan, trans_eQTLGen_FDR_05, on = ["predChr", "obsgene"])
+MultiXcan_match_eQTLGen.loc[MultiXcan_match_eQTLGen.rsid_pos <= (MultiXcan_match_eQTLGen.predS1 - 1000000), ['rsid_pos']] = None #keep if w/in +/- 1 Mb https://stackoverflow.com/questions/19226488/change-one-value-based-on-another-value-in-pandas
+MultiXcan_match_eQTLGen.loc[MultiXcan_match_eQTLGen.rsid_pos >= (MultiXcan_match_eQTLGen.predS2 + 1000000), ['rsid_pos']] = None
+MultiXcan_match_eQTLGen = MultiXcan_match_eQTLGen[np.isfinite(MultiXcan_match_eQTLGen['rsid_pos'])] #keep SNPs that pass
+MultiXcan_match_eQTLGen = MultiXcan_match_eQTLGen[["eQTLGen_P", "rsid", "predChr", "rsid_pos", "eQTLGen_gene_chr", "eQTLGen_gene_pos", "eQTLGen_gene_symbol", "eQTLGen_FDR", "predgene", "predname", "predS1", "obsgene", "obsname", "obsS1", "obsS2", "FHS_pval", "FHS_FDR", "DGN_pval"]]
+MultiXcan_match_eQTLGen.to_csv("MultiXcan_match_eQTLGen.csv", index = False)
